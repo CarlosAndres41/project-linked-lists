00f4cd31f9df2af24a5618aad30ada47b5c5397f
Carlos Andrés <ca.castaneda@outlook.com>
2023-03-28 17:49:13 -0500

    Create append method

--- linked-lists.js
+++ linked-lists.js
@@ -7,4 +7,18 @@ class Node {
 
 const linkedList = (startingNode) => {
     let headNode = startingNode;
+
+    // Add a new node containing value to the end of the list
+    let append = (newNode) => {
+        let current = headNode;
+        while (current !== null) {
+            if (current.next === null) {
+                current.next = newNode;
+                return;
+            }
+            current = current.next;
+        }
+    };
+
+    return { append };
 };
